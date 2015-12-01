3289fe7955606a92fd22e1c98ed2d8bafaef7893
Apavayan Sinha <info@apavayan.com>
2015-12-01 16:22:36 +0530

    Merged INDIA_BUILD of S5 MINI firmware and add busybox

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-#if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:9646080:0b94c97e5cbcac8124b41faaeabeb86b93162586; then
-#  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:8781824:6cefc425c9e450432d2bb856f55362e4c0a8f375 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery #0b94c97e5cbcac8124b41faaeabeb86b93162586 9646080 6cefc425c9e450432d2bb856f55362e4c0a8f375:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery #"Installing new recovery image: failed"
-#else
-#  log -t recovery "Recovery image already installed"
-#fi
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:9646080:0b94c97e5cbcac8124b41faaeabeb86b93162586; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:8781824:6cefc425c9e450432d2bb856f55362e4c0a8f375 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 0b94c97e5cbcac8124b41faaeabeb86b93162586 9646080 6cefc425c9e450432d2bb856f55362e4c0a8f375:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+else
+  log -t recovery "Recovery image already installed"
+fi
